c9dc72876619c697421635191a80aaa97c2408d4
firdaws1234 <nazarovfirdavs12341234@gmail.com>
2023-05-13 15:48:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Blocke Scope in Java script 
+
+sum=0
+for(let i=0; i<=a; i++){               
+    
+    sum+=i
+}
+console.log(12);     
+
+
+ 
+if(a<1 && b>1){
+    a=true
+    b=false
+}
+  console.log(1,2);
